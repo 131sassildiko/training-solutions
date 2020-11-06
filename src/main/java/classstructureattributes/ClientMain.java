528c226cf56150fd04a65021f906bf2e7581a622
131sassildiko <sass.ildiko@freemail.hu>
2020-11-06 23:39:57 +0100

    regist

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,21 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Client client = new Client();
+
+            System.out.println("what's your name?");
+            client.name = scanner.nextLine();
+            System.out.println("what's your year of birth?");
+            client.birth = scanner.nextInt();
+            System.out.println("what's your address?");
+            client.address = scanner.nextLine();
+            System.out.println("client.name" + "clients.birth" + "client.address");
+        }
+    }
+
+}
