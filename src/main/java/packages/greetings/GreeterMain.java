207c367f83bc22abce3751e6c3f8b45ce4d34ef1
131sassildiko <sass.ildiko@freemail.hu>
2020-11-19 20:55:05 +0100

    new

---
+++ src/main/java/packages/greetings/GreeterMain.java
@@ -0,0 +1,9 @@
+package packages.greetings;
+
+public class GreeterMain {
+    public static void main(String[] args) {
+        Greeter greeter  = new Greeter();
+        greeter.sayHello();
+
+    }
+}
