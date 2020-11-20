deca9f6e90307c22eb22c901a7b01170da6a7c4b
131sassildiko <sass.ildiko@freemail.hu>
2020-11-20 21:26:27 +0100

    new

---
+++ src/main/java/introdate/EmployeeTestMain.java
@@ -0,0 +1,13 @@
+package introdate;
+
+public class EmployeeTestMain {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Pali", 1972, 01, 30 );
+        System.out.println(employee.getBeginEmployment());
+        System.out.println(employee.getDateOfBirth());
+    }
+
+
+}
