ecaec9d9ebc6409ad3744312a90db68ebbe78a68
131sassildiko <sass.ildiko@freemail.hu>
2020-12-11 11:36:46 +0100

    new

--- src/main/java/array/ArrayMain.java
+++ src/main/java/array/ArrayMain.java
@@ -6,12 +6,22 @@ public class ArrayMain {
         System.out.println(daysOfWeek [1]);
         System.out.println(daysOfWeek.length);
 
-        int[] kettohatvany = {1, 2, 4, 8, 16};
+        System.out.println();
+
+        int[] kettohatvany = new int[5];
+        kettohatvany [0] = 1;
+        for (int i=1; i<kettohatvany.length; i++){
+            kettohatvany [i] = kettohatvany [i-1]*2;
+                    }
         for (int item: kettohatvany){
             System.out.println(item);
         }
+        System.out.println();
 
-        boolean[] akarmi = {false, true, false, true, false, true};
+        boolean[] akarmi = new boolean[6];
+        for (int i=1; i< akarmi.length; i++) {
+            akarmi [i] = ! akarmi [i-1];
+        }
 
         for (boolean item: akarmi) {
             System.out.println(item);
