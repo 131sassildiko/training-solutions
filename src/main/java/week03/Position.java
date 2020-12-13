69a6f488d7f13bae15cad98f56d6ab987ff3efba
131sassildiko <sass.ildiko@freemail.hu>
2020-12-13 20:31:21 +0100

    new

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,51 @@
+package week03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "name:'" + name + '\'' +
+                ", bonus=" + bonus +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        List<Position> positions = new ArrayList<>();
+
+        positions.add(new Position("Manager", 16000000));
+        positions.add(new Position("Boss", 19000000));
+        positions.add(new Position("Worker", 1600));
+
+
+        for (Position i:positions) {
+            if (i.getBonus()>150000){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+    }
+}
