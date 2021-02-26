4b017de23aad533fe10ba07f4da7696f78cd7a6e
131sassildiko <sass.ildiko@freemail.hu>
2021-02-26 21:09:32 +0100

    new

--- src/main/java/exam03retake02/BalatonStorm.java
+++ src/main/java/exam03retake02/BalatonStorm.java
@@ -3,26 +3,29 @@ package exam03retake02;
 import java.awt.dnd.DropTarget;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BalatonStorm {
 
-
-    private List <String> StormList = new ArrayList<>();
-
-    public List<String> getStationsInStorm(BufferedReader reader) throws IOException {
-        String line;
-        while ((line = reader.readLine()) !=null){
-            System.out.println(line);
-        }
+    public static void main(String[] args) {
 
 
-    public Temp (String line){
-            id=Integer.parseInt(line.split (":") [1]);
-            allomas=line.split(":")
-        }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    BalatonStorm.class.getResourceAsStream("/storm.json")))) {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            catch (IOException ioe) {
+                throw new IllegalStateException("Can not read file", ioe);
+            }
+
+
 
 
     }
 }
+
